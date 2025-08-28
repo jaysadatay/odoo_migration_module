1d41d32f3832ce22cf2230a2ce74a36b492470f1
jaysadatay <joerghueneburg@gmail.com>
2025-08-28 20:39:24 +0200

    manifest.py changes

--- __manifest__.py
+++ __manifest__.py
@@ -1,20 +1,18 @@
 # -*- coding: utf-8 -*-
 #################################################################################
-# Author      : Anderson Martinez (andersonvidal94@gmail.com)
-# License     : AGPL-3
-#
+
 #################################################################################
 {
     'name': 'Migration from other odoo',
-    "version": "1.0.0",
-    "author": "Anderson Martinez",
-    "maintainer": "Anderson Martinez",
-    #'depends': ['queue_job'],
+    "version": "14.0.0.0",
+    "author": "MeLikeyMedia",
+    "maintainer": "MeLikeyMedia",
+    'depends': ['queue_job'],
     'external_dependencies': {'python': ['odoorpc']},
     'data': [
-        'data/migration.credentials.csv',
-        # 'data/migration.model.csv',
-        # 'data/migration.record.csv',
+        #'data/migration.credentials.csv',
+        #'data/migration.model.csv',
+        #'data/migration.record.csv',
         'security/ir.model.access.csv',
         'views/migration_views.xml',
     ],
